b2ba37e7d9b6075a9ea7374241c581f8afb58e9b
Raposo4 <raposolizandro@gmail.com>
2023-02-07 17:54:39 -0300

    Renomeando arquivos por motivos de organização

--- atv011.py
+++ atv011.py
@@ -1,5 +1,5 @@
 #Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pinta-la, sabendo que 
-#cada litro de tinta pinta uma área de 2m².
+#cada litro de tinta pinta uma área de
 
 larg = float(input('Largura da parede: '))
 alt = float(input('Altura da parede: '))
