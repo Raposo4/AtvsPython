f1c680cff054baec3d80d10daa736503f964718a
Raposo4 <raposolizandro@gmail.com>
2023-08-28 15:20:54 -0300

    Att

--- atv023.py
+++ atv023.py
@@ -7,7 +7,8 @@ print(f'Unidade: {numero[len(numero)-1]}')
 if(len(numero)-2 >= 0):
     print(f'Dezena: {numero[len(numero)-2]}')
 
-if(len(numero)-3 >= 0 and (int(numero[len(numero)-3]) != 0 and not (int(numero[len(numero)-4]) != 0))):
+if(len(numero)-3 >= 0):
+    
     print(f'Centena: {numero[len(numero)-3]}')
 
 if(len(numero)-4 >= 0 and int(numero[len(numero)-4]) != 0):
